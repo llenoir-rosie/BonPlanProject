e456d26f5052fc93b795fec63017fc0c74ac26ea
Gabriel PONS <gpons@consultants-solutec.fr>
2023-03-27 09:35:28 +0200

    commentaire

--- src/main/java/ProjetBonPlan/DemoApplication.java
+++ src/main/java/ProjetBonPlan/DemoApplication.java
@@ -6,15 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @ComponentScan("ProjetBonPlan.*")
 @EnableJpaRepositories(basePackages = { "ProjetBonPlan.*" })
 @EntityScan("ProjetBonPlan.model")  
 @SpringBootApplication
 public class DemoApplication {
-	//implements CommandLineRunner
+	//implements CommandLineRunner 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
